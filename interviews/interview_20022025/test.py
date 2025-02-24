521a2f4277c9e04160778c3c9716769aaa55fb20
Pratyusha <44306139+gudiausha@users.noreply.github.com>
2025-02-24 19:07:46 +0530

    [update] practise

---
+++ interviews/interview_20022025/test.py
@@ -0,0 +1,40 @@
+# PS Mock Interview1 - BofA - Equities Trade Management Team
+# def two_sum(inp_lst,t):
+#     i  = 0
+#     j = len(inp_lst)-1
+
+#     while i<j:
+#         if inp_lst[i]+inp_lst[j] == t:
+#             return (inp_lst[i], inp_lst[j])
+#         i+=1
+#         j-=1
+#     return -1
+
+# output = two_sum([-2,-3,4,5],1)
+# print(output)
+
+# def simple_gen(iterable):
+#     for num in range(iterable):
+#         yield num
+
+# # print(list(simple_gen(10)))
+# output = next(simple_gen(10))
+# print(output)
+
+# from abc import abstractmethod
+
+# Serialization, also known as marshaling 
+# Operator Overloading
+# Abstraction
+# Pandas, numpy 
+# Green Threads
+# asyncio 
+# SOLID 
+# ACID
+# CAP theorem
+# unique key, composite key, primary key 
+# Joins
+# abstractmethod
+# classmethod
+
+# Queue, Pipe, Value, Array, Manager
